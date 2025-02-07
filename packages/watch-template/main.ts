010908b21e747a76305b65a530b1d4325bfd2559
Aliver <aliver.len@outlook.com>
2025-02-08 01:17:23 +0800

    optimize: watcher error handling

--- packages/watch-template/main.ts
+++ packages/watch-template/main.ts
@@ -9,6 +9,7 @@ import {
 } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { mainnet } from "viem/chains";
+import { Watcher } from "./watcher";
 
 /**
  * Watch for a specific event on a contract
@@ -26,56 +27,58 @@ const config = {
   abiOfContractToCall: erc721Abi,
 };
 
-async function watch() {
-  const transport = config.socketURL
-    ? webSocket(config.socketURL)
-    : http(config.rpcURL);
+class MyWatcher extends Watcher {
+  protected watchEvent() {
+    const me = createWalletClient({
+      chain: config.network,
+      transport: this.transport,
+      account: privateKeyToAccount(config.pk),
+    });
 
-  const client = createPublicClient({
-    chain: config.network,
-    transport: transport,
-  });
-  const me = createWalletClient({
-    chain: config.network,
-    transport: transport,
-    account: privateKeyToAccount(config.pk),
-  });
+    const unwatch = this.client.watchContractEvent({
+      address: config.contractAddressToWatch,
+      abi: [config.abiOfEventToWatch],
+      onError: this.onError,
+      onLogs: async (logs) => {
+        // TODO: Do something with the logs
+        console.log(logs);
 
-  client.watchContractEvent({
-    address: config.contractAddressToWatch,
-    abi: [config.abiOfEventToWatch],
-    onLogs: async (logs) => {
-      // TODO: Do something with the logs
-      console.log(logs);
+        // Example: Call a contract function
+        const contract = getContract({
+          abi: config.abiOfContractToCall,
+          address: config.contractAddressToCall,
+          client: { public: this.client, wallet: me },
+        });
 
-      // Example: Call a contract function
-      const contract = getContract({
-        abi: config.abiOfContractToCall,
-        address: config.contractAddressToCall,
-        client: { public: client, wallet: me },
-      });
+        const txHash = await contract.write.transferFrom([
+          me.account.address,
+          "0x",
+          logs[0].args.tokenId ?? 0n,
+        ]);
 
-      const txHash = await contract.write.transferFrom([
-        me.account.address,
-        "0x",
-        logs[0].args.tokenId ?? 0n,
-      ]);
+        console.log(`Call ${config.contractAddressToCall} txHash: ${txHash}`);
+        const receipt = await this.client.waitForTransactionReceipt({
+          hash: txHash,
+        });
+        console.table({
+          hash: txHash,
+          block: receipt.blockNumber,
+          gasUsed: formatUnits(
+            receipt.gasUsed,
+            config.network.nativeCurrency.decimals
+          ),
+        });
+      },
+    });
 
-      console.log(`Call ${config.contractAddressToCall} txHash: ${txHash}`);
-      const receipt = await client.waitForTransactionReceipt({ hash: txHash });
-      console.table({
-        hash: txHash,
-        block: receipt.blockNumber,
-        gasUsed: formatUnits(
-          receipt.gasUsed,
-          config.network.nativeCurrency.decimals
-        ),
-      });
-    },
-    onError: (error) => {
-      console.error(error);
-    },
-  });
+    return unwatch;
+  }
 }
 
-watch().catch(console.error);
+const watcher = new MyWatcher({
+  chain: config.network,
+  rpcURL: config.rpcURL,
+  socketURL: config.socketURL,
+});
+
+watcher.watch();
